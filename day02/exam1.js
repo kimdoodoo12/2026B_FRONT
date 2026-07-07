1064fe3cbc636fb19bc80cbda97c21e8c051b351
kimdoodoo12 <kimdoodoo12@gmail.com>
2026-07-07 15:34:51 +0900

    first commit

---
+++ day02/exam1.js
@@ -0,0 +1,25 @@
+
+
+10 // 숫자
+"유재석" // 문자열
+true // 논리
+{ } // 객체
+[ ] //
+function fx() {} //
+undefined //
+null //
+
+console.log("wsg")
+
+// 변수: 하나의 자료 저장하나는 메모리(컴퓨터 저장 단위) 공간
+// 1. let 2. 변수 이름 
+let ten = 100
+
+console.log(ten)
+
+ten = 200
+console.log(ten)
+
+// 상수 정의 (수정불가)
+// 1. const 2.초기값 필수
+const twoHundred = 200
