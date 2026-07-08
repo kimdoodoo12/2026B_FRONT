d7084f79d8e7ca0f3faa0e6cede41b1e802ad74a
kimdoodoo12 <kimdoodoo12@gmail.com>
2026-07-08 15:54:33 +0900

    3:54PM

--- day03/practice01.js
+++ day03/practice01.js
@@ -1,44 +1,44 @@
-// let korean = prompt("국어 점수: ")
-// let english = prompt("영어 점수: ")
-// let math = prompt("수학 점수: ")
-// korean = parseInt(korean)
-// english = parseInt(english)
-// math = parseInt(math)
+let korean = prompt("국어 점수: ")
+let english = prompt("영어 점수: ")
+let math = prompt("수학 점수: ")
+korean = parseInt(korean)
+english = parseInt(english)
+math = parseInt(math)
 
-// let total_score = 0
-// total_score += korean
-// total_score += english
-// total_score += math
+let total_score = 0
+total_score += korean
+total_score += english
+total_score += math
 
-// let average_score = 0
-// average_score = total_score/3
-// console.log("총점: " + total_score + " 평균점수: " + average_score)
-
-
-// let r = prompt("반지름 길이 입력: ")
-// r = parseFloat(r)
-// console.log(`원넓이: ${r*r*3.14}`)
-
-// let a = prompt("두 실수를 입력하시오").split(" ").map(Number)
-// console.log(a[0]/a[1])
-
-// let b = Number(prompt("정수를 입력하시오"))
-// console.log((b%7==0) ? true : false)
+let average_score = 0
+average_score = total_score/3
+console.log("총점: " + total_score + " 평균점수: " + average_score)
 
 
-// let user_id = prompt("ID: ")
-// let user_password = prompt("Password")
+let r = prompt("반지름 길이 입력: ")
+r = parseFloat(r)
+console.log(`원넓이: ${r*r*3.14}`)
 
-// console.log(Boolean(user_id == "admin" && user_password == "1234"))
+let a = prompt("두 실수를 입력하시오").split(" ").map(Number)
+console.log(a[0]/a[1])
+
+let b = Number(prompt("정수를 입력하시오"))
+console.log((b%7==0) ? true : false)
 
 
-// let c = Number(prompt("정수를 입력하시오"))
-// console.log(`${Boolean(c%7==0 || c%2==1)}`)
+let user_id = prompt("ID: ")
+let user_password = prompt("Password")
+
+console.log(Boolean(user_id == "admin" && user_password == "1234"))
 
 
-// let first_score = Number(prompt("1차 점수: "))
-// let second_score = Number(prompt("2차 점수: "))
-// console.log(`${(first_score + second_score > 150 ? "합격" : "불합격")}`)
+let c = Number(prompt("정수를 입력하시오"))
+console.log(`${Boolean(c%7==0 || c%2==1)}`)
+
+
+let first_score = Number(prompt("1차 점수: "))
+let second_score = Number(prompt("2차 점수: "))
+console.log(`${(first_score + second_score > 150 ? "합격" : "불합격")}`)
 
 
 let name1 = prompt("이름1: ")
